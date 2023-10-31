18d49b54c2019fc941081ff1148b935071832e07
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-10-31 12:33:04 +0300

    GuessTheNumber

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,16 +1,17 @@
 public class PrimeNumber {
     public static void main(String[] args) {
-        int j=0;
-        int [] arr = new int[2];
-        for (int i = 2; i <=100 ; i++) {
-            for (int k = 0; k >0; k--) {
-                if (i%j==0){
-                    arr[k]=i;
-                    k++;
+        for (int i = 2; i < 100; i++) {
+            boolean isTrue = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isTrue = false;
+                    break;
                 }
-
             }
-
+            if (isTrue) {
+                System.out.println(i);
+            }
         }
     }
 }
+
