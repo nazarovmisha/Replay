94d8b75a5f560972cd1be9b08950ec08d3279329
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-11-03 09:53:10 +0300

    IsPow

--- IsPow.java
+++ IsPow.java
@@ -1,16 +1,20 @@
 public class IsPow {
     public static void isPow(int n) {
         int count = 0;
-        while (n > 2) {
+        int start = 1;
+        while (start < n) {
             count++;
-            n=n/n;
-            System.out.println(n);
+            start *= 2;
+        }
+        if (start == n) {
+            System.out.println(count);
+        } else {
+            System.out.println("NO");
         }
     }
 
-
     public static void main(String[] args) {
-        isPow(64);
+        isPow(1024);
     }
 }
 
