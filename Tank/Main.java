767cd12796b467ea26b7b4560dc88b22bb142cea
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-11-14 12:49:59 +0300

    Tank

--- Tank/Main.java
+++ Tank/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Tank tank = new Tank();
         tank.goForward(10);
         tank.printPosition();
+        tank.turnRight();
         tank.goForward(50);
         tank.printPosition();
+        tank.turnLeft();
         tank.goBackward(100);
         tank.printPosition();
-
 
     }
 }
--- Tank/Tank.java
+++ Tank/Tank.java
@@ -2,17 +2,55 @@ package Tank;
 
 public class Tank {
 
-    static int position = 0;
+    static int positionX = 0;
+    static int positionY = 0;
+    static int reverse = 0;
+
 
     public static void goForward(int x) {
-        position += x;
+
+        if (reverse == 0) {
+            positionY += x;
+        } else if (reverse == 1) {
+            positionX += x;
+        } else if (positionX == 3) {
+            positionY -= x;
+        } else positionY -= x;
     }
 
     public static void printPosition() {
-        System.out.println("The Tank is at " + position + " now.");
+        System.out.println("The Tank is at " + positionY + "," + positionX + " now.");
     }
 
     public static void goBackward(int x) {
-         position -= x;
+        if (reverse == 0) {
+            positionY -= x;
+        } else if (reverse == 1) {
+            positionX -= x;
+        } else if (positionX == 3) {
+            positionY += x;
+        } else positionY += x;
     }
+
+
+    public static void turnRight() {
+        reverse += 1;
+        if (reverse == 4) {
+            reverse = 0;
+        }
+        if (reverse == -1) {
+            reverse = 3;
+        }
+    }
+
+    public static void turnLeft() {
+        reverse -= 1;
+        if (reverse == 4) {
+            reverse = 0;
+        }
+        if (reverse == -1) {
+            reverse = 3;
+        }
+    }
+
 }
