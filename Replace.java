bb4603fc7db8dc0d8cb30791540e96494c788d22
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-11-17 11:19:01 +0300

    Replace

---
+++ Replace.java
@@ -0,0 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Replace {
+    public static void main(String[] args) {
+
+
+        String text = "Иван Петров";
+        Pattern pattern = Pattern.compile("(.*) (.*)");
+        Matcher matcher = pattern.matcher(text);
+        System.out.println(matcher.replaceFirst("$2 $1"));
+    }
+}
