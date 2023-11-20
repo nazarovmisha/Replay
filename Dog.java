5a265368d9783add64ce354bc2f8f475d0c5606c
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-11-20 14:39:10 +0300

    Dog

---
+++ Dog.java
@@ -0,0 +1,13 @@
+public class Dog {
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void bark() {
+        System.out.println("Woof");
+    }
+}
