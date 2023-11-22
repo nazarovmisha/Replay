9f37347e36e14942ef8498d5ec61b6c6a06c6777
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-11-22 11:26:56 +0300

    ToTable

---
+++ ToTable.java
@@ -0,0 +1,11 @@
+public class ToTable {
+    int[]data;
+    int x;
+    int y;
+
+    public ToTable(int[] data, int x, int y) {
+        this.data = data;
+        this.x = x;
+        this.y = y;
+    }
+}
