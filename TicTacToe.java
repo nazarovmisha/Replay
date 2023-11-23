7cf06836dc7f510e9555c439d11eb7732062fc18
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-11-23 14:46:55 +0300

    Tic

---
+++ TicTacToe.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class TicTacToe {
+    String[][] array;
+
+    void newGame() {
+    array=new String[3][3];
+        Arrays.fill(array, "-");
+
+
+     }
+    }
+
