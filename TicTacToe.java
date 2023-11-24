72f495a36398053fa409a7198efded217aaebdbd
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-11-24 13:59:49 +0300

    Tic

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,12 +2,25 @@ import java.util.Arrays;
 
 public class TicTacToe {
     String[][] array;
+    String nowPlayer;
 
     void newGame() {
     array=new String[3][3];
         Arrays.fill(array, "-");
-
-
      }
+      String[][] getField(){
+        return array;
+      }
+
+      String checkGame(){
+          for (int i = 0; i < 3; i++) {
+              for (int j = 0; j < 3; j++) {
+                  if (array[i][j].equals("x")){
+                      return "x";
+                  }
+
+              }
+          }
+      }
     }
 
