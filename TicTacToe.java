8d69631dde54d4f29086f02a688ac5ea349d8e7f
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-11-27 14:51:29 +0300

    Tic

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,26 +1,43 @@
 import java.util.Arrays;
+import java.util.TooManyListenersException;
 
 public class TicTacToe {
     String[][] array;
     String nowPlayer;
 
     void newGame() {
-    array=new String[3][3];
-        Arrays.fill(array, "-");
-     }
-      String[][] getField(){
-        return array;
-      }
+        array = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                array[i][j]="x";
+            }
+        }
+       for (String[] i:array){
 
-      String checkGame(){
-          for (int i = 0; i < 3; i++) {
-              for (int j = 0; j < 3; j++) {
-                  if (array[i][j].equals("x")){
-                      return "x";
-                  }
 
-              }
-          }
-      }
+       }
     }
+
+    String[][] getField() {
+        return array;
+    }
+
+    String checkGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (array[i][j].equals("x")) {
+                    return "x";
+                }
+
+            }
+        }return "a";
+    }
+
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.newGame();
+
+    }
+}
+
 
