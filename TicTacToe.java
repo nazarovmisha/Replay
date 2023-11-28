68da6401abc2a472e99ed9abfcce665ef815eaf0
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-11-28 13:37:17 +0300

    Tic

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,20 +3,19 @@ import java.util.TooManyListenersException;
 
 public class TicTacToe {
     String[][] array;
-    String nowPlayer;
+    int nowPlayer;
 
     void newGame() {
         array = new String[3][3];
+        nowPlayer = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                array[i][j]="x";
+                array[i][j] = "-";
             }
         }
-       for (String[] i:array){
-
-
-       }
+        System.out.println(Arrays.deepToString(array).replace("], ", "]\n"));
     }
+
 
     String[][] getField() {
         return array;
@@ -28,15 +27,28 @@ public class TicTacToe {
                 if (array[i][j].equals("x")) {
                     return "x";
                 }
-
             }
-        }return "a";
+        }
+        return "a";
+    }
+
+    void makeMove(int x, int y) {
+        if (array[x - 1][y - 1].equals("X") || array[x - 1][y - 1].equals("0")) {
+            System.out.println("Cell" + "[" + x + "]" + "[" + y + "] is already occupied");
+
+        } else {
+            if (nowPlayer % 2 == 0) {
+                array[x - 1][y - 1] = "x";
+            } else {
+                array[x - 1][y - 1] = "y";
+            }
+        }
+        nowPlayer++;
     }
 
     public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.newGame();
-
     }
 }
 
