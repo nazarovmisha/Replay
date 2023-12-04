1cf81f15407ccf4a212825b7ba86d7f081b9d086
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-12-04 14:38:16 +0300

    Tic

--- TicTacToe.java
+++ TicTacToe.java
@@ -37,26 +37,36 @@ public class TicTacToe {
     }
 
     boolean checkGame() {
+        String[] horizontal1 = {array[0][0], array[0][1], array[0][2]};
+        String[] horizontal2 = {array[1][0], array[1][1], array[1][2]};
+        String[] horizontal3 = {array[2][0], array[2][1], array[2][2]};
+        String[] vertical1 = {array[0][0], array[1][0], array[2][0]};
+        String[] vertical2 = {array[0][1], array[1][1], array[2][1]};
+        String[] vertical3 = {array[0][2], array[1][2], array[2][2]};
+        String[] diagonal = {array[0][0], array[1][1], array[2][2]};
+        String[] diagonal2 = {array[0][2], array[1][1], array[2][0]};
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.println("i=" + i + " j=" + j);
-                if (array[i][j].equals("0") || array[j][i].equals("-")) {
+
+                if (horizontal1[i].equals("x") || horizontal2[i].equals("x") || horizontal3[i].equals("x") || vertical1[i].equals("x")
+                        || vertical2[i].equals("x") || vertical3[i].equals("x") || diagonal[i].equals("x") || diagonal2[i].equals("x")) {
                     System.out.println("x win");
                     return false;
-                } else if (array[j][i].equals("0") || array[j][j].equals("-")) {
-                    System.out.println("x win");
+                } else if (horizontal1[i].equals("0") || horizontal2[i].equals("0") || horizontal3[i].equals("0") || vertical1[i].equals("0")
+                        || vertical2[i].equals("0") || vertical3[i].equals("0") || diagonal[i].equals("0") || diagonal2[i].equals("0")) {
+                    System.out.println("0 win");
+                    return false;
+                } else if (!array[i][j].equals("-")) {
+                    System.out.println("Draw!");
                     return false;
 
-                }else if(array[i][i].equals("0")||array[i][i].equals("-")){
-                    System.out.println("x win");
-                    return false;
                 }
             }
-        }return true;
+        }
+        return true;
     }
 
     void makeMove(int x, int y) {
-
         if (array[x - 1][y - 1].equals("x") || array[x - 1][y - 1].equals("0")) {
             System.out.println("Cell" + "[" + x + "]" + "[" + y + "] is already occupied, сходи по-другому");
             Scanner scanner = new Scanner(System.in);
@@ -74,5 +84,6 @@ public class TicTacToe {
         nowPlayer++;
     }
 }
+
 
 
