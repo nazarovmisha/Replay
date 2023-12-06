e81a4d6a7825c529978bd883dc3220187e87aa25
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-12-06 11:14:07 +0300

    Zoo

---
+++ Zoo/Main.java
@@ -0,0 +1,14 @@
+package Zoo;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Zoo zoo = new Zoo();
+        zoo.change(new Dog(), 2);
+        System.out.println(zoo);
+
+
+    }
+
+}
---
+++ Zoo/PopcornStall.java
@@ -0,0 +1,13 @@
+ package Zoo;
+
+public class PopcornStall{
+    public void print(){
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "It's a PopcornStall";
+    }
+
+}
