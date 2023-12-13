42c931f18dc5dc9821a15127735c00ad4b990972
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-12-13 13:51:28 +0300

    Player

--- Monster/Player.java
+++ Monster/Player.java
@@ -1,4 +1,32 @@
 package Monster;
 
-public class Player {
+public class Player extends Human {
+
+    public class Backpack {
+        String packages = "";
+
+        void put(String str) {
+            packages += str + ", ";
         }
+
+        public String toString() {
+            if (packages.equals("")) {
+                return "backpack is empty";
+            }
+            return packages;
+        }
+    }
+    Backpack backpack;
+
+    public Player(String name) {
+        super(name + "player");
+        backpack=new Backpack();
+    }
+
+    public void take(String str) {
+        backpack.put(str);
+    }
+
+}
+
+
