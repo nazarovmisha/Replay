9a990e9622603198dfbb1687c594635640935e8a
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-12-14 12:37:14 +0300

    Horse

---
+++ Chess/ChessPiece.java
@@ -0,0 +1,17 @@
+package Chess;
+
+abstract public class ChessPiece {
+    String color;
+    boolean check;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    abstract public String getColor();
+
+    abstract public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+    abstract public String getSymbol();
+
+}
---
+++ Chess/Horse.java
@@ -0,0 +1,24 @@
+package Chess;
+
+public class Horse extends ChessPiece {
+    public Horse(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toLine>=0&&toLine<=7&&toColumn>=0&&toColumn<=7){
+        return true;
+    }else return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "H";
+    }
+}
