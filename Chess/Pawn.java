de9f52f9b02177322cc8ee8407129c1cee214f16
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-12-14 13:24:19 +0300

    Pawn

---
+++ Chess/Pawn.java
@@ -0,0 +1,22 @@
+package Chess;
+
+public class Pawn extends ChessPiece {
+    public Pawn(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+
+    }
+
+    @Override
+    public String getSymbol() {
+        return "P";
+    }
+}
