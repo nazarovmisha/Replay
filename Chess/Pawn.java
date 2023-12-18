03d6f3505b2b66ddef58ac052c11f87cc83c5e13
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-12-18 14:47:26 +0300

    Horse

--- Chess/Pawn.java
+++ Chess/Pawn.java
@@ -12,7 +12,12 @@ public class Pawn extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-
+        if (toLine >= 0 && toLine <= 7 && toColumn >= 0 && toColumn <= 7&&t) {
+            if (((color.equals("White")&&line==1)&&(toLine==line+1||toLine==line+2))||(color.equals("Black")&&line==6)&&
+            (toLine==line-1||toLine==line-2)){
+return true;
+            }
+        }
     }
 
     @Override
