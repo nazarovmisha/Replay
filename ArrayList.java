b80df8f782b61d59cd36507fc21a568456216110
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-01-15 10:46:07 +0300

    ArrayList

---
+++ ArrayList.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        List<Integer> list = new java.util.ArrayList<>(99);
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+        for (int i = 0, j = 99; i < 50 && j >= 50; i++, j--) {
+            int start = list.get(i);
+            int finish = list.get(j);
+            list.set(j, start);
+            list.set(i, finish);
+        }
+        System.out.println(list);
+    }
+}
