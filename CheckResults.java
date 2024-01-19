39ec37bbb11b52e8656d7d483029350316e8a676
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-01-19 12:44:14 +0300

    CheckResults

--- CheckResults.java
+++ CheckResults.java
@@ -1,17 +1,17 @@
 public class CheckResults {
     private static int counter=0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new Thread(){
             public void run(){
                 for (int i = 0; i < 100; i++) {
                     CheckResults.counter++;
-
                 }
             }
         }.start();
         while (CheckResults.counter<100){
             System.out.println("Not ready");
+            Thread.sleep(1000);
         }
         System.out.println("Ready");
     }
