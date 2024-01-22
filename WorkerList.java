a51e95e7c0069372fe210851f7b40f1672304dac
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-01-22 13:30:07 +0300

    WorkerList

---
+++ WorkerList.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkerList {
+    private List<String> list1 = new ArrayList<>();
+    private List<String> list2 = new ArrayList<>();
+
+    synchronized void addOne() {
+        for (int i = 0; i < 100000; i++) {
+            list1.add("One");
+        }
+    }
+
+    synchronized void addTwo() {
+        for (int i = 0; i < 100000; i++) {
+            list2.add("Two");
+        }
+    }
+
+    class Process extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 300; i++) {
+                addOne();
+                addTwo();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WorkerList wl = new WorkerList();
+        Thread t1 = wl.new Process();
+        Thread t2 = wl.new Process();
+
+        long start = System.currentTimeMillis();
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        long finish = System.currentTimeMillis();
+
+        System.out.println("List 1 size is:" + wl.list1.size());
+        System.out.println("List 2 size is:" + wl.list2.size());
+        System.out.println("Time taken " + (finish - start) + "ms");
+    }
+}
+
