7fec5254f454c73918b32bb5608517c87fdcb9ea
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-01-22 11:05:45 +0300

    CalculatePerimeters

---
+++ CalculatePerimeters.java
@@ -0,0 +1,12 @@
+public class CalculatePerimeters {
+    static double sideTriangle = 3;
+
+    public static void main(String[] args) {
+
+        System.out.println(perimeterTriangle(sideTriangle));
+    }
+
+    public static double perimeterTriangle(double d) {
+        return d * 3;
+    }
+}
