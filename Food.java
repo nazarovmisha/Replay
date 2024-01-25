8546fd625a90fa256ef66e8911c581806884b3f7
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-01-25 10:20:06 +0300

    Food

--- Food.java
+++ Food.java
@@ -1,16 +1,15 @@
-
-import java.util.HashMap;
 import java.util.Map;
-
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Food {
     public static void main(String[] args) {
-        Map<String, Object> foodData = new HashMap<>();
+        Map<String, Object> foodData = new ConcurrentHashMap<>();
         foodData.put("penguin", 1);
         foodData.put("flamingo",2);
         System.out.println(foodData);
         for (Object key:foodData.keySet()){
            foodData.remove(key);
         }
+        System.out.println(foodData);
     }
 }
