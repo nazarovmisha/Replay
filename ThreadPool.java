4cbda672e077c54e89f81e3514a651ca5ff5fe5b
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-01-26 10:51:09 +0300

    ThreadPool

---
+++ ThreadPool.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPool {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 5; i++)
+            executorService.submit(new Work(i));
+        executorService.shutdown();
+        System.out.println("All tasks was submitted");
+    }
+}
+
+class Work implements Runnable {
+    private int id;
+
+    public Work(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Work " + id + " was completed");
+    }
+}
+
