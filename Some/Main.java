4503740a88010c3e980cfd6986701fd6fc30f39e
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-02-02 13:31:46 +0300

    Validator

--- Some/Main.java
+++ Some/Main.java
@@ -3,16 +3,17 @@ package Some;
 public class Main {
     public static void main(String[] args) {
         int value = 70;
-        validate(value, value1 -> value1 >=20);
+        validate(value, value1 -> value1 % 2 == 0);
+        validate(value, value1 -> value1 >= 100);
     }
 
     public static void validate(int value, Validator validator) {
-        System.out.println("Проверяем переданоое значение");
+        System.out.println("Проверяем переданоое значение " + value);
         boolean isValid = validator.validate(value);
         if (isValid) {
-            System.out.println("Отлично, значение верно");
+            System.out.println("Отлично, значение " + value + " верно");
         } else {
-            System.out.println("Значение верно");
+            System.out.println("Значение не верно");
         }
     }
 }
