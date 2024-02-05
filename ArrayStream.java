a481faf1d8d3df4004a641139778c0fc284e4fc4
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-02-05 12:42:29 +0300

    ArrayStream

---
+++ ArrayStream.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayStream {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        list.stream().map((x) -> x * x).forEach(System.out::println);
+    }
+}
+
