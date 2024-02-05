13a909356438689cd3956e37e49c90fb976bc41b
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-02-05 12:45:08 +0300

    ArrayStream

--- ArrayStream.java
+++ ArrayStream.java
@@ -4,7 +4,8 @@ import java.util.List;
 public class ArrayStream {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        list.stream().map((x) -> x * x).forEach(System.out::println);
+        int sum = list.stream().map((x) -> x * x).reduce(Integer::sum).get();
+        System.out.println(sum);
     }
 }
 
