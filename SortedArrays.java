1d316064bf3993c8546178be96df9b3f5bcb7857
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-02-07 09:42:07 +0300

    SortedArrays

--- SortedArrays.java
+++ SortedArrays.java
@@ -1,22 +1,9 @@
 import java.util.*;
-import java.util.ArrayList;
+
 
 public class SortedArrays {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("Toby", "Anna", "Leroy", "Alex");
-        List<String> filtred = new ArrayList<>();
-        for (String s : list) {
-            if (s.length() == 4) {
-                filtred.add(s);
-            }
-        }
-        Collections.sort(filtred);
-        Iterator<String> iter = filtred.iterator();
-        if (iter.hasNext()) {
-            System.out.println(iter.next());
-        }
-        if (iter.hasNext()) {
-            System.out.println(iter.next());
-        }
+        list.stream().filter(n -> n.length() == 4).sorted().limit(2).forEach(System.out::println);
     }
 }
