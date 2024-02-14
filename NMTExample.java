7bb18c6f25b88e892072d48498f6a2b8104b287a
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-02-14 14:26:47 +0300

    NMTExample

---
+++ NMTExample.java
@@ -0,0 +1,15 @@
+public class NMTExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                private byte[] data;
+                @Override
+                public void run() {
+                    while (true) {
+                        data = new byte[1024 * 1024];
+                    }
+                }
+            }, "thread" + i).start();
+        }
+    }
+}
