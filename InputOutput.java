8514087ad064a8702bb537d5c10d6801e7020173
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-05-21 14:53:41 +0300

    Wrighter

---
+++ InputOutput.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class InputOutput {
+
+    /*
+	Для чтения входных данных необходимо получить их
+	из стандартного потока ввода (System.in).
+	Данные во входном потоке соответствуют описанному
+	в условии формату. Обычно входные данные состоят
+	из нескольких строк. Можно использовать более производительные
+	и удобные классы BufferedReader, BufferedWriter, Scanner, PrintWriter.
+
+	С помощью BufferedReader можно прочитать из стандартного потока:
+	* строку -- reader.readLine()
+	* число -- int n = Integer.parseInt(reader.readLine());
+	* массив чисел известной длины (во входном потоке каждое число на новой строке) --
+	int[] nums = new int[len];
+    for (int i = 0; i < len; i++) {
+        nums[i] = Integer.parseInt(reader.readLine());
+    }
+	* последовательность слов в строке --
+	String[] parts = reader.readLine().split(" ");
+
+	Чтобы вывести результат в стандартный поток вывода (System.out),
+	Через BufferedWriter можно использовать методы
+	writer.write("Строка"), writer.write('A') и writer.newLine().
+
+	Возможное решение задачи "Вычислите сумму чисел в строке":
+	int sum = 0;
+    String[] parts = reader.readLine().split(" ");
+    for (int i = 0; i < parts.length; i++) {
+        int num = Integer.parseInt(parts[i]);
+        sum += num;
+    }
+    writer.write(String.valueOf(sum));
+	*/
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] parts = reader.readLine().split(" ");
+        int[] arr = new int[parts.length];
+        for (int i = 0; i < parts.length; i++) {
+            arr[i] = Integer.parseInt(parts[i]);
+        }
+        Arrays.sort(arr);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        writer.write(arr[1]);
+        writer.flush();
+        reader.close();
+        writer.close();
+    }
+}
